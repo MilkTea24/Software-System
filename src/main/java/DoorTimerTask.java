fcf8af2f21ebfd72866c0b763e098ec9fdbaf980
MilkTea24 <kimmanjae15@pusan.ac.kr>
2024-05-13 19:24:05 +0900

    Initial commit

---
+++ src/main/java/DoorTimerTask.java
@@ -0,0 +1,15 @@
+
+
+import java.util.TimerTask;
+
+class DoorTimerTask extends TimerTask {
+	private EC doorTimeout;
+
+	public void run() {
+		doorTimeout.doorTimeout() ;
+	}
+
+	public void setDoorTimeout(EC doorTimeout) {
+		this.doorTimeout = doorTimeout;
+	}	
+}
